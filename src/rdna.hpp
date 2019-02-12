cf8196d221297509173b27fe97db101332919e03
comieb832 <comieb832@users.noreply.github.com>
2019-02-12 19:58:01 -0400

    work in progress

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,7 +1,24 @@
 #include <iostream>
-#include <string>
-#include <randnum>
+#include <rand0m>
 
 using namespace std;
 
-string randnum 
+string randDNA(int seed, string basese, intn)
+
+{
+	mt19937 eng1(seed);
+	
+	int min = 0;
+	int max = bases.size()-1;
+	uniform_int_distrubution<>uniform(min,max);
+	int index = 0;
+	string dna = " ";
+	for (int i = 0; i < n; i++)
+	
+	{
+		index = uniform(eng1);
+		dna = dna + bases[index];
+		
+	}
+	return dna;
+}
